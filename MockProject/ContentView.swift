fd5fc69043750fe18084fdeeab6c520379195b10
Jared Davidson (Archetapp) <archetapp@gmail.com>
2021-11-06 12:26:52 -0600

    Initial Commit

---
+++ MockProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MockProject
+//
+//  Created by Jared Davidson on 11/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
